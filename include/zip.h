dcfc2a00cf2dc52890438c1a0842e3fd31fe4d44
MacPapo <jacopocostantini32@gmail.com>
2025-06-19 19:24:13 +0200

    Updated functions
    Added GPL3 License and zip macros

--- include/zip.h
+++ include/zip.h
@@ -4,16 +4,56 @@
 #include <stdint.h>
 #include <stdio.h>
 
-/* CODES */
-#define ZIP_OK                 0
-#define ZIP_ERR_EOCD_NOT_FOUND 1
-#define ZIP_ERR_EOCD_INVALID   2
-#define ZIP_ERR_INVALID_SIG    3
-#define ZIP_ERR_MEM_ALLOC      4
+/* --- ZIP Library Error Codes --- */
+
+/* Success code */
+#define ZIP_OK 0
+
+/*
+ * System/Resource Errors (reflects underlying system call failures)
+ */
+#define ZIP_ERR_IO_READ        1 /* Failed to read data from the file. */
+#define ZIP_ERR_IO_WRITE       2 /* Failed to write data to the file. */
+#define ZIP_ERR_IO_SEEK        3 /* Failed to change file offset (lseek error). */
+#define ZIP_ERR_MEM_ALLOC      4 /* Memory allocation failed (out of memory). */
+#define ZIP_ERR_INVALID_ARG    5 /* Invalid argument provided (e.g., NULL pointer). */
+#define ZIP_ERR_BAD_FILE_DESC  6 /* The provided file descriptor is invalid. */
+#define ZIP_ERR_FILE_TOO_SMALL 7 /* ZIP file too small for basic structures. */
+#define ZIP_ERR_FILE_TRUNCATED 8 /* An expected amount of data could not be read. */
+
+
+/*
+ * ZIP Format Specific Errors (related to parsing ZIP structures)
+ */
+/* EOCD (End of Central Directory) related errors */
+#define ZIP_ERR_EOCD_NOT_FOUND           10 /* EOCD record (signature 0x06054b50) not found. */
+#define ZIP_ERR_EOCD_SIGNATURE_BAD       11 /* EOCD found, but its signature is incorrect. */
+#define ZIP_ERR_EOCD_CORRUPT_FIELDS      12 /* EOCD fields inconsistent or invalid. */
+
+/* Central Directory related errors */
+#define ZIP_ERR_CENTRAL_DIR_LOC        20 /* Failed to seek to Central Directory start. */
+#define ZIP_ERR_CENTRAL_DIR_READ       21 /* Failed to read Central Directory data. */
+#define ZIP_ERR_CENTRAL_DIR_CORRUPT    22 /* Central Directory contents malformed. */
+#define ZIP_ERR_CD_ENTRY_SIGNATURE_BAD 23 /* CD file header entry has incorrect signature. */
+#define ZIP_ERR_CD_ENTRY_CORRUPT       24 /* CD file header entry fields are corrupted. */
+
+/* Local File Header related errors */
+#define ZIP_ERR_LFH_LOC           30 /* Failed to seek to a Local File Header. */
+#define ZIP_ERR_LFH_READ          31 /* Failed to read a Local File Header. */
+#define ZIP_ERR_LFH_SIGNATURE_BAD 32 /* LFH has an incorrect signature. */
+#define ZIP_ERR_LFH_CORRUPT       33 /* LFH fields are corrupted. */
+
+/* Decompression/Compression related errors (for future expansion) */
+#define ZIP_ERR_COMPRESSION_UNSUPPORTED 50 /* Compression method not supported. */
+#define ZIP_ERR_DECOMPRESSION_FAILED    51 /* Decompression process failed. */
+
+/* Generic/Fallback error */
+#define ZIP_ERR_GENERIC        99 /* A general, unclassified error occurred. */
 
 /* SIGNATURES */
-#define CENTRAL_FILE_HEADER_SIGNATURE 0x02014b50
-#define EOCD_SIGNATURE                0x06054b50
+#define EOCD_SIGNATURE 0x06054b50
+#define LFD_SIGNATURE  0x06054b50
+#define CFH_SIGNATURE  0x02014b50
 
 #define EOCD_MAX_COMMENT_LEN 0xFFFF
 #define EOCD_FIXED_SIZE      22
--- src/zip.c
+++ src/zip.c
@@ -1,10 +1,32 @@
+/*
+ * zip.c -- Zip Reader
+ * Copyright (C) 2025 Jacopo Costantini
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "zip.h"
+#include <assert.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
 #include <stdlib.h>
 
+/* End of Central Directory */
 struct EOCD {
-        uint32_t signature;
+        uint32_t signature;     /* EOCD_SIGNATURE */
         uint16_t this_disk;
         uint16_t central_dir_disk;
         uint16_t total_entries_this_disk;
@@ -15,13 +37,77 @@ struct EOCD {
         char* comment;
 };
 
-off_t
-find_eocd(const int_fast32_t fd)
+/* Local File Header */
+struct LFH {
+        uint32_t signature;     /* LFH_SIGNATURE */
+        uint16_t version;
+        uint16_t bit_flag;
+        uint16_t comp_method;
+        uint16_t last_mod_file_time;
+        uint16_t last_mod_file_date;
+        uint32_t crc32;
+        uint32_t comp_size;
+        uint32_t uncomp_size;
+        uint16_t file_name_len;
+        uint16_t extra_field_len;
+        char* file_name;
+        char* field;
+};
+
+const char*
+get_zip_error_message(int code) {
+    switch (code) {
+        case ZIP_OK: return "No error";
+        case ZIP_ERR_IO_READ: return "Failed to read data from the file";
+        case ZIP_ERR_IO_WRITE: return "Failed to write data to the file";
+        case ZIP_ERR_IO_SEEK: return "Failed to change file offset";
+        case ZIP_ERR_MEM_ALLOC: return "Memory allocation failed";
+        case ZIP_ERR_INVALID_ARG: return "Invalid argument provided";
+        case ZIP_ERR_BAD_FILE_DESC: return "Bad file descriptor";
+        case ZIP_ERR_FILE_TOO_SMALL: return "ZIP file is too small or invalid";
+        case ZIP_ERR_FILE_TRUNCATED: return "File ended prematurely or incomplete read";
+
+        case ZIP_ERR_EOCD_NOT_FOUND: return "End of Central Directory record not found";
+        case ZIP_ERR_EOCD_SIGNATURE_BAD: return "Incorrect EOCD signature";
+        case ZIP_ERR_EOCD_CORRUPT_FIELDS: return "EOCD fields are corrupted or inconsistent";
+
+        case ZIP_ERR_CENTRAL_DIR_LOC: return "Failed to seek to Central Directory";
+        case ZIP_ERR_CENTRAL_DIR_READ: return "Failed to read Central Directory data";
+        case ZIP_ERR_CENTRAL_DIR_CORRUPT: return "Central Directory data is corrupted";
+        case ZIP_ERR_CD_ENTRY_SIGNATURE_BAD: return "Central Directory entry has incorrect signature";
+        case ZIP_ERR_CD_ENTRY_CORRUPT: return "Central Directory entry fields are corrupted";
+
+        case ZIP_ERR_LFH_LOC: return "Failed to seek to Local File Header";
+        case ZIP_ERR_LFH_READ: return "Failed to read Local File Header";
+        case ZIP_ERR_LFH_SIGNATURE_BAD: return "Local File Header has incorrect signature";
+        case ZIP_ERR_LFH_CORRUPT: return "Local File Header fields are corrupted";
+
+        case ZIP_ERR_COMPRESSION_UNSUPPORTED: return "Compression method not supported";
+        case ZIP_ERR_DECOMPRESSION_FAILED: return "Decompression failed";
+
+        case ZIP_ERR_GENERIC: return "An unclassified generic error occurred";
+        default: return "Unknown ZIP error code";
+    }
+}
+
+uint8_t
+find_eocd(const int_fast32_t fd, off_t* eocd_pos_out)
 {
+        if (fd < 0) {
+                return ZIP_ERR_BAD_FILE_DESC;
+        }
+
+        if (!eocd_pos_out) {
+                return ZIP_ERR_INVALID_ARG;
+        }
+
         off_t file_pos = lseek(fd, 0, SEEK_END);
         if (file_pos == -1) {
-                perror("LSEEK (SEEK_END)");
-                exit(EXIT_FAILURE);
+                return ZIP_ERR_IO_SEEK;
+        }
+
+        if (file_pos < EOCD_FIXED_SIZE) {
+                return ZIP_ERR_FILE_TOO_SMALL;
         }
 
         off_t search_start = file_pos - EOCD_FIXED_SIZE - EOCD_MAX_COMMENT_LEN;
@@ -32,84 +118,89 @@ find_eocd(const int_fast32_t fd)
         if (read_size > EOCD_MAX_COMMENT_LEN + EOCD_FIXED_SIZE)
                 read_size = EOCD_MAX_COMMENT_LEN + EOCD_FIXED_SIZE;
 
-        unsigned char buff[EOCD_MAX_COMMENT_LEN + EOCD_FIXED_SIZE + 1];
+        uint8_t buf[EOCD_MAX_COMMENT_LEN + EOCD_FIXED_SIZE + 1];
 
-        if (lseek(fd, search_start, SEEK_SET) == -1) {
-                perror("LSEEK (SEEK_SET)");
-                exit(EXIT_FAILURE);
-        }
+        if (lseek(fd, search_start, SEEK_SET) == -1)
+                return ZIP_ERR_IO_SEEK;
 
-        ssize_t bytes_read = read(fd, buff, read_size);
-        if (bytes_read < 0) {
-                perror("READ");
-                exit(EXIT_FAILURE);
-        }
+        ssize_t bytes_read = read(fd, buf, read_size);
+        if (bytes_read < 0)
+                return ZIP_ERR_IO_READ;
+
+        if ((size_t)bytes_read < EOCD_FIXED_SIZE)
+                return ZIP_ERR_FILE_TRUNCATED;
 
         for (off_t i = bytes_read - EOCD_FIXED_SIZE; i >= 0; --i) {
-                uint32_t sig =  (uint32_t)buff[i]             /* 0x00000050 */
-                             | ((uint32_t)buff[i + 1] << 8)   /* 0x00004b00 */
-                             | ((uint32_t)buff[i + 2] << 16)  /* 0x00050000 */
-                             | ((uint32_t)buff[i + 3] << 24); /* 0x06000000 */
+                uint32_t sig =  (uint32_t)buf[i]             /* 0x00000050 */
+                             | ((uint32_t)buf[i + 1] << 8)   /* 0x00004b00 */
+                             | ((uint32_t)buf[i + 2] << 16)  /* 0x00050000 */
+                             | ((uint32_t)buf[i + 3] << 24); /* 0x06000000 */
 
 
-                if (sig == EOCD_SIGNATURE)
-                        return search_start + i;
+                if (sig == EOCD_SIGNATURE) {
+                        *eocd_pos_out = search_start + i;
+                        return ZIP_OK;
+                }
         }
 
-        return -1;
+        return ZIP_ERR_EOCD_NOT_FOUND;
 }
 
-int8_t
+uint8_t
 read_eocd(const int_fast32_t fp, off_t eocd_pos, struct EOCD* eocd_out)
 {
-        unsigned char buff[EOCD_FIXED_SIZE];
+        if (fp < 0)
+                return ZIP_ERR_BAD_FILE_DESC;
 
-        if (lseek(fp, eocd_pos, SEEK_SET) == -1) {
-                perror("lseek to EOCD");
-                return -1;
-        }
+        if (!eocd_out)
+                return ZIP_ERR_INVALID_ARG;
 
-        if (read(fp, buff, EOCD_FIXED_SIZE) != EOCD_FIXED_SIZE) {
-                perror("read EOCD");
-                return -1;
-        }
+        eocd_out->comment = NULL;
+        uint8_t buf[EOCD_FIXED_SIZE];
+        if (lseek(fp, eocd_pos, SEEK_SET) == -1)
+                return ZIP_ERR_IO_SEEK;
+
+        ssize_t bytes_read = read(fp, buf, EOCD_FIXED_SIZE);
+        if (bytes_read != EOCD_FIXED_SIZE)
+                return ZIP_ERR_FILE_TRUNCATED;
 
         eocd_out->signature =
-                   (uint32_t)buff[0]
-                | ((uint32_t)buff[1] << 8)
-                | ((uint32_t)buff[2] << 16)
-                | ((uint32_t)buff[3] << 24);
+                   (uint32_t)buf[0]
+                | ((uint32_t)buf[1] << 8)
+                | ((uint32_t)buf[2] << 16)
+                | ((uint32_t)buf[3] << 24);
+        if (eocd_out->signature != EOCD_SIGNATURE)
+                return ZIP_ERR_EOCD_SIGNATURE_BAD;
 
-        eocd_out->this_disk = buff[4] | (buff[5] << 8);
-        eocd_out->central_dir_disk = buff[6] | (buff[7] << 8);
-        eocd_out->total_entries_this_disk = buff[8] | (buff[9] << 8);
-        eocd_out->total_entries = buff[10] | (buff[11] << 8);
+        eocd_out->this_disk = buf[4] | (buf[5] << 8);
+        eocd_out->central_dir_disk = buf[6] | (buf[7] << 8);
+        eocd_out->total_entries_this_disk = buf[8] | (buf[9] << 8);
+        eocd_out->total_entries = buf[10] | (buf[11] << 8);
 
         eocd_out->central_dir_size =
-                   buff[12]
-                | (buff[13] << 8)
-                | (buff[14] << 16)
-                | (buff[15] << 24);
+                   buf[12]
+                | (buf[13] << 8)
+                | (buf[14] << 16)
+                | (buf[15] << 24);
 
         eocd_out->central_dir_offset =
-                   buff[16]
-                | (buff[17] << 8)
-                | (buff[18] << 16)
-                | (buff[19] << 24);
+                   buf[16]
+                | (buf[17] << 8)
+                | (buf[18] << 16)
+                | (buf[19] << 24);
 
-        eocd_out->comment_length = buff[20] | (buff[21] << 8);
-
+        eocd_out->comment_length = buf[20] | (buf[21] << 8);
         if (eocd_out->comment_length > 0) {
                 eocd_out->comment = malloc(eocd_out->comment_length + 1);
                 if (!eocd_out->comment) {
-                        perror("malloc comment");
-                        return -1;
+                        return ZIP_ERR_MEM_ALLOC;
                 }
 
-                if (read(fp, eocd_out->comment, eocd_out->comment_length) != eocd_out->comment_length) {
-                        perror("read comment");
+                bytes_read = read(fp, eocd_out->comment, eocd_out->comment_length);
+                if (bytes_read != eocd_out->comment_length) {
                         free(eocd_out->comment);
-                        return -1;
+                        eocd_out->comment = NULL;
+                        return ZIP_ERR_FILE_TRUNCATED;
                 }
 
                 eocd_out->comment[eocd_out->comment_length] = '\0';
@@ -117,28 +208,54 @@ read_eocd(const int_fast32_t fp, off_t eocd_pos, struct EOCD* eocd_out)
                 eocd_out->comment = NULL;
         }
 
-        return 0;
+        return ZIP_OK;
 }
 
 uint8_t
 zip_read_directory(const int_fast32_t fp, struct ZipEntry* entries[], uint32_t* entry_count)
 {
-        off_t eocd_pos = find_eocd(fp);
-        if (eocd_pos == -1) {
-                fprintf(stderr, "NO EOCD FOUND: Error %d\n", ZIP_ERR_EOCD_NOT_FOUND);
-                return ZIP_ERR_EOCD_NOT_FOUND;
+        if (fp < 0) {
+                fprintf(stderr, "Error: Invalid file descriptor provided.\n");
+                return ZIP_ERR_BAD_FILE_DESC;
+        }
+
+        if (!entries || !entry_count) {
+                fprintf(stderr, "Error: Invalid arguments (entries or entry_count) provided.\n");
+                return ZIP_ERR_INVALID_ARG;
+        }
+
+        off_t eocd_pos;
+        uint8_t err = find_eocd(fp, &eocd_pos);
+        if (err != ZIP_OK) {
+                fprintf(stderr, "Error finding EOCD: %s\n", get_zip_error_message(err));
+                if (err >= ZIP_ERR_IO_READ && err <= ZIP_ERR_FILE_TRUNCATED)
+                        fprintf(stderr, "  System error details: %s\n", strerror(errno));
+
+                return (uint8_t)err;
         }
 
         struct EOCD eocd;
-        if (read_eocd(fp, eocd_pos, &eocd) != 0) {
-                fprintf(stderr, "Failed to read EOCD\n");
-                return ZIP_ERR_EOCD_INVALID;
+        eocd.comment = NULL;
+
+        err = read_eocd(fp, eocd_pos, &eocd);
+        if (err != ZIP_OK) {
+                fprintf(stderr, "Error reading EOCD at offset %ld: %s\n", (long)eocd_pos, get_zip_error_message(err));
+                if (err >= ZIP_ERR_IO_READ && err <= ZIP_ERR_FILE_TRUNCATED)
+                        fprintf(stderr, "  System error details: %s\n", strerror(errno));
+
+                if (eocd.comment) {
+                        free(eocd.comment);
+                        eocd.comment = NULL;
+                }
+                return (uint8_t)err;
         }
 
         if (eocd.comment) {
+                printf("%s\n\n", eocd.comment);
+
                 free(eocd.comment);
                 eocd.comment = NULL;
         }
 
-        return 0;
+        return ZIP_OK;
 }
